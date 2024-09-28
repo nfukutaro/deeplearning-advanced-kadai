3be4a78294ebd0c7ca4e498f38700341eda9d05f
nfukutaro <naoyuki.fukuju@gmail.com>
2024-09-29 07:24:15 +0900

    kadai_06を追加

---
+++ kadai_06/prediction/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from .forms import ImageUploadForm
+from django.conf import settings
+from tensorflow.keras.applications.vgg16 import VGG16
+model = VGG16(weights='imagenet')
+
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.applications.vgg16 import preprocess_input
+from tensorflow.keras.applications.vgg16 import decode_predictions
+
+from io import BytesIO
+import os
+
+def predict(request):
+    if request.method == 'GET':
+        #GETリクエストによるアクセス時の処理を記述
+        form = ImageUploadForm()
+        return render(request, 'home.html', {'form': form})
+    if request.method == 'POST':
+        form = ImageUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            img_file = form.cleaned_data['image']
+            img_file = BytesIO(img_file.read())
+            img = load_img(img_file, target_size=(224,224))
+            img_array = img_to_array(img)
+            img_array = img_array.reshape((1, 224, 224,3))
+            img_array = preprocess_input(img_array)
+            model_path = os.path.join(settings.BASE_DIR, 'prediction','models','vgg16.h5')
+            model =load_model(model_path)
+
+            predictions = model.predict(img_array)  # 予測実行
+            decoded_predictions = decode_predictions(predictions, top=5)[0]  # 予測結果をデコード
+
+            # パーセンテージに変換
+            formatted_predictions = [(pred[1], f"{pred[2] * 100:.2f}") for pred in decoded_predictions]
+            img_data = request.POST.get('img_data')
+            return render(request, 'home.html', {'form': form, 'prediction': formatted_predictions, 'img_data':img_data})
+        else:
+            form = ImageUploadForm()
+            return render(request, 'home.html', {'form':form})
+
+
